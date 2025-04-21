09db93d24a73979f623945639d155017d7b588e0
Anukriti Kashyap <kashyapanukriti1@gmail.com>
2025-04-21 18:41:50 +0530

    Initial commit

---
+++ ecom-user-service/src/test/java/com/ecom/ecom_user_service/EcomUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecom_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
